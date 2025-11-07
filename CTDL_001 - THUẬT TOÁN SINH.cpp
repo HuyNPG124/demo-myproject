47011032979e20d5b1772190e84bd1efaf199951
HuyNPG124 <HuyNPG.B23DT124@stu.ptit.edu.vn>
2025-11-07 16:48:07 +0700

    Refactor comments and initialization in sinh algorithm

--- CTDL_001 - THUẬT TOÁN SINH.cpp
+++ CTDL_001 - THUẬT TOÁN SINH.cpp
@@ -1,33 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 // thuat toan sinh
-//- Cau hinh dau tien 
-// cau hinh cuoi cung
-// phuong phap sinh
-//<Khoi tao cau hinh dau tien>
-//while(<Khi chua phai cau hinh cuoi cung>){
-//	<In ra cau hinh hien tai>
-//	<Sinh ra cau hinh tiep theo>
-//}
 int n, a[100], ok;
 void ktao(){
 	for(int i = 1; i <= n; i++){
-		a[i] = 0;//
+		a[i] = 0; //<Khoi tao cau hinh dau tien>
 	}
 }
+//<Khoi tao cau hinh dau tien>
 void sinh(){
 	int i = n;
-	while( i >= 1 && a[i] == 1){
+	while( i >= 1 && a[i] == 1){ //while(<Khi chua phai cau hinh cuoi cung>){
 		a[i] = 0;
 		--i;
 	}
 	if(i == 0){
-		ok = 0;
+		ok = 0; // kiem tra xem co phai cau hinh cuoi cung khong?
 	}
 	else{
 		a[i] = 1;
 	}
 }
+//kiem tra thuan nghich
 bool check(){
 	int l = 1, r = n;
 	while(l <= r){
@@ -46,10 +40,10 @@ int main(){
 	while(ok){
 		if(check()){
 			for(int i = 1; i <=n; i++){
-				cout << a[i] << " ";
+				cout << a[i] << " "; //	<In ra cau hinh hien tai>
 		}
 	cout << endl;
 	}
-	sinh();
+	sinh();//	<Sinh ra cau hinh tiep theo
 	}
 }
