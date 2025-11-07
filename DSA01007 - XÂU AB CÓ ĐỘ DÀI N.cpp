a4fc1397f79fec0c1312fb9bd06118744639669b
HuyNPG124 <HuyNPG.B23DT124@stu.ptit.edu.vn>
2025-11-07 22:48:00 +0700

    Create DSA01007 - XÂU AB CÓ ĐỘ DÀI N.cpp
    Implement a program to generate binary strings of length N consisting of characters 'A' and 'B'.

---
+++ DSA01007 - XÂU AB CÓ ĐỘ DÀI N.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int ok; int n, a[1001];
+void ktao(){
+	cin >> n;
+	for(int i = 1; i <= n; i++){
+		a[i] = 0;
+	}
+}
+void sinh(){
+	int i = n;
+	while(i >= 1 && a[i] == 1){ 
+		a[i] = 0;
+		i--;
+	}
+	if(i == 0){
+		ok = 0;
+	}
+	else{
+		 a[i] = 1;
+		}
+}
+
+
+int main(){
+	int t; cin >> t;
+	while(t--){
+		ok = 1;
+		ktao();
+		while(ok){
+		for(int i = 1; i <= n; i++){
+			if(a[i]){
+			cout << 'B';
+			}
+			else cout <<'A';
+		}
+			cout << " ";
+		sinh();
+		}
+		cout << "\n";
+	}
+}
+  
