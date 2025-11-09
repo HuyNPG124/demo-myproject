f88819f877fdaf1cc8b1df401c7a47ce50c74559
HuyNPG124 <HuyNPG.B23DT124@stu.ptit.edu.vn>
2025-11-09 11:01:25 +0700

    Update Bài 7: Ký tự lặp.cpp

--- Bài 7: Ký tự lặp.cpp
+++ Bài 7: Ký tự lặp.cpp
@@ -16,12 +16,13 @@ void Try(int i, int cou) {
             hv[i] = a[j];
 
             // Nhánh cận
-            int dd[127] = {};
-            for (char c : hv[i]) dd[c]++;
-            for (char c : hv[i - 1]) dd[c]++;
+            int dd[127] = {};// Mảng đếm tần suất ký tự ASCII (đủ cho chữ cái 'A'–'Z').
+            for (char c : hv[i]) dd[c]++; //Cộng 1 cho từng ký tự xuất hiện trong chuỗi hiện tại.
+            for (char c : hv[i - 1]) dd[c]++;//Cộng 1 cho từng ký tự xuất hiện trong chuỗi trước đó.
             int dem = 0;
             for (int i = 'A'; i <= 'Z'; ++i) {
-                if (dd[i] == 2) dem++;
+                if (dd[i] == 2) dem++; 
+            //Nếu một ký tự có giá trị 2 → tức là xuất hiện trong cả hai chuỗi (mỗi chuỗi 1 lần). Mỗi ký tự trùng nhau như vậy được tính là 1 chi phí.
             }
             if (cou + dem < ans)
                 Try(i + 1, cou + dem);
